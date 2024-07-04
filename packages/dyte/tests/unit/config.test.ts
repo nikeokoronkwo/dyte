dc1c9bca6a3dcfb648d9ff3810e96aa98d585031
Nikechukwu <nikechukwu@gmail.com>
2024-07-03 22:17:38 -0400

    config tests

---
+++ packages/dyte/tests/unit/config.test.ts
@@ -0,0 +1,18 @@
+import { assert, assertEquals, assertStrictEquals, assertExists } from "https://deno.land/std@0.224.0/assert/mod.ts";
+import { generateConfig } from "../../src/config/config.ts";
+
+Deno.test("Dev Config Tests", {}, async (t) => {
+    await t.step("Case I", () => {
+        const config = generateConfig("development", ".", "foo");
+
+        assertStrictEquals(config.root, "foo");
+        assertEquals(config.mode, "development");
+        assertStrictEquals(config.base, "/");
+        assert(config.npm === undefined);
+        assertEquals(config.server, { port: 8000, host: "localhost" })
+    });
+});
+
+Deno.test("Production Config Tests", {}, (t) => {
+
+})
