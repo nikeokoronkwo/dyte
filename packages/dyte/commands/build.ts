11a1e4621424a43e95eec2a30692d103ff1ce11a
--add <nikechukwu@gmail.com>
2024-07-17 17:57:47 -0400

    made basic changes

--- packages/dyte/commands/build.ts
+++ packages/dyte/commands/build.ts
@@ -1,18 +1,28 @@
-import { Command, join } from "../deps.ts";
+import { Command, join, exists } from "../deps.ts";
 import { generateConfig, getConfiguration } from "../src/config/config.ts";
 import { DenoConfig, DenoFile } from "../src/options/DenoConfig.ts";
 import { createBundleOptions } from "../src/cli/createBundleOptions.ts";
+import { bundle } from "../src/bundle.ts";
+import { resolveEntry } from "../src/cli/resolveEntry.js";
+import { hash } from "../src/cli/hash.js"
 
 export default new Command()
   .option("-o --output <out>", "Output Directory for Code")
+  .option("-e --entry <file>", "The entry JavaScript/TypeScript file for the project")
   .arguments("[directory]")
   .action(async (options, args) => {
     await buildCommand(options, args);
   });
 
-async function buildCommand(options: any, args?: string) {
+async function buildCommand(options: {
+  output?: string | undefined;
+  entry?: string | undefined;
+}, args?: string) {
   const denoCwd = Deno.cwd();
   const outDir = join(denoCwd, options?.output ?? "dist");
+
+  Deno.mkdirSync(outDir, { recursive: true });
+  
   // get cwd
   const cwd = join(denoCwd, args ?? ".");
 
@@ -29,5 +39,11 @@ async function buildCommand(options: any, args?: string) {
     true,
   );
 
-  // const output = bundle();
+  let resolvedEntry = await resolveEntry(cwd, appConfig);
+  let entryMap = resolvedEntry.map ?? new Map([[options.entry, `${(await hash(options.entry)).slice(0, 10)}.js`]]);
+  
+  for (const [entry, out] of Array.from(entryMap.entries())) {
+    const b = await bundle(join(appConfig.root ?? cwd, entry), bundleOptions);
+    Deno.writeTextFileSync(join(outDir, out), b);
+  };
 }
