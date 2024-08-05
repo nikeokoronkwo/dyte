4d8c520d9fad8402efaba12779a466c079ea1626
nikechukwu <nikechukwu@googlemail.com>
2024-08-05 19:28:13 +0000

    updated dependencies

--- packages/dyte/deps.ts
+++ packages/dyte/deps.ts
@@ -1,17 +1,31 @@
-export { Command, EnumType } from "jsr:@cliffy/command@1.0.0-rc.5";
 export { loadConfig, watchConfig } from "npm:c12";
 export type { UserInputConfig } from "npm:c12";
+
 export { join } from "jsr:@std/path/join";
 export { delay } from "jsr:@std/async/delay";
 export { toFileUrl } from "jsr:@std/path/to-file-url";
 export { SEPARATOR } from "jsr:@std/path/constants";
 export { existsSync, exists } from "jsr:@std/fs/exists";
 export { normalize } from "jsr:@std/path/normalize";
+export { encodeHex } from "jsr:@std/encoding/hex";
+export { extname } from "jsr:@std/path@^0.223.0/extname";
+export {
+  blue,
+  bold,
+  gray,
+  green,
+  magenta,
+  red,
+  yellow,
+} from "jsr:@std/fmt/colors";
+
 export { bundle, transpile } from "jsr:@deno/emit";
-export { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
-export { encodeHex } from "jsr:@std/encoding/hex"
 export type {
   BundleOptions,
   CompilerOptions,
   TranspileOptions,
 } from "jsr:@deno/emit";
+export { Command, EnumType } from "jsr:@cliffy/command@1.0.0-rc.5";
+
+export * as esbuild from "https://deno.land/x/esbuild/mod.js";
+export { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
